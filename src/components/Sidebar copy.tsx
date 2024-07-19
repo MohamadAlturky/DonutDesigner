156dc0ab19b45d7ea0c87308b1cd54366e2bc02d
mohamad.alturky <m799678@gmail.com>
2024-07-19 17:11:18 +0300

    sidebar styling

--- src/components/Sidebar copy.tsx
+++ src/components/Sidebar copy.tsx
@@ -10,68 +10,68 @@ const onDragStart = (event: DragEvent, nodeType: string) => {
 const Sidebar = () => {
   return (
     <aside className={styles.aside}>
-      <div id="side-bar-link-1" className={styles.dash}>Components</div>
-      <div id="side-bar-link-2" className={styles.dashLink} onDragStart={(event: DragEvent) => onDragStart(event, 'input')} draggable>
+      <div id="side-bar-link-1" className={styles.dash}>Dashboard</div>
+      <div id="side-bar-link-2" className="react-flow__node-input" onDragStart={(event: DragEvent) => onDragStart(event, 'input')} draggable>
         Input Node
       </div>
       <div id="side-bar-link-3"
-        className={styles.dashLink}
+        className="react-flow__node-default"
         onDragStart={(event: DragEvent) => onDragStart(event, 'default')}
         draggable
       >
         Default Node
       </div>
       <div id="side-bar-link-4"
-        className={styles.dashLink}
+        className="react-flow__node-default"
         onDragStart={(event: DragEvent) => onDragStart(event, 'annotation')}
         draggable
       >
         annotation
       </div>
       <div id="side-bar-link-4-gateway"
-        className={styles.dashLink}
+        className="react-flow__node-default"
         onDragStart={(event: DragEvent) => onDragStart(event, 'gateway')}
         draggable
       >
         gateway
       </div>
       <div id="side-bar-link-5"
-        className={styles.dashLink}
+        className="react-flow__node-default"
         onDragStart={(event: DragEvent) => onDragStart(event, 'pool')}
         draggable
       >
         pool
       </div>
       <div id="side-bar-link-6"
-        className={styles.dashLink}
+        className="react-flow__node-default"
         onDragStart={(event: DragEvent) => onDragStart(event, 'textinput')}
         draggable
       >
         textinput
       </div>
       <div id="side-bar-link-15"
-        className={styles.dashLink}
+        className="react-flow__node-default"
         onDragStart={(event: DragEvent) => onDragStart(event, 'activity')}
         draggable
       >
         activity
       </div>
       <div id="side-bar-link-7"
-        className={styles.dashLink}
+        className="react-flow__node-default"
         onDragStart={(event: DragEvent) => onDragStart(event, 'circle')}
         draggable
       >
         circle
       </div>
       <div id="side-bar-link-8"
-        className={styles.dashLink}
+        className="react-flow__node-output"
         onDragStart={(event: DragEvent) => onDragStart(event, 'output')}
         draggable
       >
         Output Node
       </div>
       <div id="side-bar-link-9"
-        className={styles.dashLink}
+        className="react-flow__node-output"
         onDragStart={(event: DragEvent) => onDragStart(event, 'group')}
         draggable
       >
